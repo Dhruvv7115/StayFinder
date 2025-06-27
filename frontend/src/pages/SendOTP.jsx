9d45ab6a1e2e5c66395c26b0d2353c2aeaf5de2b
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2025-06-27 19:54:44 +0530

    otp ui

--- frontend/src/pages/SendOTP.jsx
+++ frontend/src/pages/SendOTP.jsx
@@ -1,18 +1,26 @@
 import { useEmailContext } from '../context/EmailContext'
-import { House } from 'lucide-react';
+import { House, MailCheck, MailX } from 'lucide-react';
 import { api } from '../Api/api';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function SendOTP() {
   const { email } = useEmailContext();
   const navigate = useNavigate();
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
 
   const handleSendOTP = async() => {
     try {
       const data = await api.sendOTP(email);
       if(data === 200) {
-        navigate("/verify-otp");}
+        setSuccess(true);
+        setTimeout(() => {
+          navigate("/verify-otp");
+        }, 3000)
+      }
     } catch (error) {
+      setError(true);
       console.error("Error sending OTP:", error);
     }
   }
@@ -36,6 +44,20 @@ function SendOTP() {
           <button 
           onClick={handleSendOTP}
           className="bg-rose-500 text-white py-2 px-4 rounded-md hover:bg-rose-600 cursor-pointer transition-colors font-semibold focus:ring-red-700 focus:ring-4">Send OTP</button>
+          {success && (
+            <div className="text-lg bg-green-900 opacity-10 px-2 py-1 rounded text-green-500">
+              <MailCheck className="inline-block ml-2" />
+              <span>A 6 digit otp sent to {email} successfully</span>
+            </div>
+          )}
+          {error && (
+            <div className="text-lg bg-red-900 opacity-10 px-2 py-1 rounded text-red-500">
+              <MailX className="inline-block ml-2" stroke="currentColor"/>
+              <span className="inline-block">An Error occured while sending {email} 
+                <span className="inline-block">resendOTP</span>
+              </span>
+            </div>
+          )}
         </div>
       </div>
     </div>
