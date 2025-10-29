cf394f7e563392ae3af2e524843c5296e2e569e4
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2025-10-30 03:15:31 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -11,11 +11,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-  })
-);
+app.use(cors());
 
 import userRouter from "./routes/User.js";
 import listingRouter from "./routes/Listing.js";
